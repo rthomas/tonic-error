f164d81eaaab5090327d582bdd4257215ac3fc7f
Ryan Thomas <ryan@ryant.org>
2022-07-02 18:20:21 +1000

    Update docs.

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@ pub use tonic_error_impl::*;
 
 static CUSTOM_ERROR: &str = "x-custom-tonic-error";
 
+/// Represents an error in converting a custom error type into a
+/// `tonic::Status`.
 #[derive(Debug, Error)]
 pub enum TonicErrorError {
     #[error("serde_json error: {0}")]
@@ -23,19 +25,34 @@ pub enum TonicErrorError {
     InvalidStatusCode(tonic::Code),
 }
 
+/// A trait to convert a custom error type into a `tonic::Status` and back
+/// again. The custom error is serialized to json using `serde_json` and stored
+/// in the `Status` metadata.
+///
+/// The `tonic::Code::Internal` is used when converting to the `Status`, and
+/// required when converting from a `Status`. A
+/// `TonicErrorError::InvalidStatusCode` will be returned if the `Status` is any
+/// other code. A `TonicErrorError::ErrorKeyNotFound` will be returned if the
+/// data is not present in the `Status` metadata map.
 pub trait TonicError<'de>: Serialize + Deserialize<'de> + Display {
+    /// Convert this type into a `tonic::Status` by serializing it into the
+    /// metadata map. If `serde_json` fails, or if the `String` does not parse
+    /// into a `MetadataValue<Ascii>` then this will be reported in the `Status`
+    /// itself as an internal error.
     fn to_status(&self) -> Status {
         let metadata_val = match serde_json::to_string(&self) {
             Ok(s) => match s.parse() {
                 Ok(m) => m,
                 Err(e) => {
                     return Status::internal(format!(
-                        "error creating metadata value from previous error: {e}"
+                        "error creating metadata value from previous error: {e} - {self}"
                     ))
                 }
             },
             Err(e) => {
-                return Status::internal(format!("error converting previous error to json: {e}"))
+                return Status::internal(format!(
+                    "error converting previous error to json: {e} - {self}"
+                ))
             }
         };
 
@@ -45,6 +62,9 @@ pub trait TonicError<'de>: Serialize + Deserialize<'de> + Display {
         status
     }
 
+    /// Deserialize this type out of a `tonic::Status` metadata field. Errors
+    /// will be returned if the `Status` is not `Code::Internal` or if there is
+    /// no data present in the metadata map that we can deserialize.
     fn from_status(s: &'de Status) -> Result<Self, TonicErrorError> {
         match s.code() {
             tonic::Code::Internal => {
